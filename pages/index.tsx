2e30ba2cc3272169c19d8dcaba58f0b983f1fcb5
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2022-07-10 18:34:29 -0300

    added action columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
-import { Typography } from "@mui/material";
+// next
 import type { NextPage } from "next";
+// material
+import { CardHeader, Grid, Card, CardContent } from "@mui/material";
+// components
 import { Layout } from "../components/layouts";
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        Hola Mundo
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="To Do" />
+            <CardContent>
+              
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Doing" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Done" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
